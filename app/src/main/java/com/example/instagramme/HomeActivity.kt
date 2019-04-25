baadebb2e732974b8c90415ce5a8fdd3e81ec424
Nicolas boue <uia81777@contiwan.com>
2019-04-25 10:03:28 +0200

    erf

--- app/src/main/java/com/example/instagramme/HomeActivity.kt
+++ app/src/main/java/com/example/instagramme/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.example.instagramme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottom_navigation_view.setTextVisibility(false);
     }
 }

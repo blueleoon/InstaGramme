04ada0d98bfc680b69249478a83b361eca4d1a52
Nicolas boue <uia81777@contiwan.com>
2019-04-25 15:29:51 +0200

    Firebase first connection working

--- app/src/main/java/com/example/instagramme/HomeActivity.kt
+++ app/src/main/java/com/example/instagramme/HomeActivity.kt
@@ -2,9 +2,11 @@ package com.example.instagramme
 
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "BaseActivity"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +14,16 @@ class HomeActivity : BaseActivity(0) {
 
         Log.d(TAG, "onCreate")
         setupbottomnavigation()
+
+
+        auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("capeselourd@auterter.com", "icicparis")
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    Log.d(TAG, "Ca va peser !")
+                } else {
+                    Log.d(TAG, "Tu postera plus tard ...", it.exception)
+                }
+            }
     }
 }

31f38ba3620baca9b6ef5306744e6d674071e517
Nicolas boue <uia81777@contiwan.com>
2019-04-25 11:13:17 +0200

    acitivties !

--- app/src/main/java/com/example/instagramme/HomeActivity.kt
+++ app/src/main/java/com/example/instagramme/HomeActivity.kt
@@ -1,22 +1,20 @@
 package com.example.instagramme
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity(0) {
+    private val TAG = "BaseActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        bottom_navigation_view.setTextVisibility(false)
-        bottom_navigation_view.enableShiftingMode(false)
-        bottom_navigation_view.enableItemShiftingMode(false)
-        bottom_navigation_view.setIconSize(29f, 29f)
-        bottom_navigation_view.enableAnimation(false)
-        for ( i in 0 until bottom_navigation_view.menu.size()){
-            bottom_navigation_view.setIconTintList(i, null)
-        }
+        Log.d(TAG, "onCreate")
+        setupbottomnavigation()
     }
 }
--- app/src/main/java/com/example/instagramme/ShareActivity.kt
+++ app/src/main/java/com/example/instagramme/ShareActivity.kt
@@ -2,21 +2,17 @@ package com.example.instagramme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_home.*
 
-class ShareActivity : AppCompatActivity() {
+class ShareActivity : BaseActivity(2) {
+    private val TAG = "ShareActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        bottom_navigation_view.setTextVisibility(false)
-        bottom_navigation_view.enableShiftingMode(false)
-        bottom_navigation_view.enableItemShiftingMode(false)
-        bottom_navigation_view.setIconSize(29f, 29f)
-        bottom_navigation_view.enableAnimation(false)
-        for ( i in 0 until bottom_navigation_view.menu.size()){
-            bottom_navigation_view.setIconTintList(i, null)
-        }
+        setupbottomnavigation()
+        Log.d(TAG, "onCreate")
     }
 }
